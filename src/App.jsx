93b17374a4851a8c51d4a2836c03070bd4a73d15
najim2004 <najim10112004@gmail.com>
2024-05-15 18:15:30 +0600

    updated app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./Shared/Navbar/Navbar";
 import Footer from "./Shared/Footer/Footer";
+import { useEffect } from "react";
 
 function App() {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
   return (
     <>
       <Navbar></Navbar>
